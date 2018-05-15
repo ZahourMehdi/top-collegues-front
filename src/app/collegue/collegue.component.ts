6742058f173bce2e63f606668998a847d8d8ff69
Mehdi.Zahour <zahour.mehdi@gmail.com>
2018-05-15 12:36:58 +0200

    Rectification erreur Collegue.ts

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -31,6 +31,7 @@ export class CollegueComponent implements OnInit {
         this.unCollegue = c;
         this.estAimer = (this.unCollegue.score >= 1000)
         this.estDetestable = (this.unCollegue.score <= -1000)
+        this.unVote.emit(new Vote($event, new Collegue(this.unCollegue.photo, this.unCollegue.pseudo, this.unCollegue.score,this.unCollegue.nom, this.unCollegue.prenom, this.unCollegue.email, this.unCollegue.adresse)))
       })
       .catch(err => console.log(err))
   }
